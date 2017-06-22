cec60f5796ffbfccaab167d7399c4543b6e89821
David Silva <zgrav@null.net>
2017-06-22 18:52:13 +0200

    ,

--- socketIO_client/transports.py
+++ socketIO_client/transports.py
@@ -143,11 +143,11 @@ class WebsocketTransport(AbstractTransport):
 
     def recv_packet(self):
         try:
-            while packet_text is None:
-                packet_text = self._connection.recv()
-                if packet_text is None:
-                    print ("zero packet")
-                    time.sleep(.05)
+            packet_text = self._connection.recv()
+            if len(str(packet_text)) is 0:
+                print "zero packet."
+                packet_text = 3
+                packet_text = packet_text.encode('utf-8')
         except WebSocketTimeoutException as e:
             raise TimeoutError('recv timed out (%s)' % e)
         except SSLError as e:
