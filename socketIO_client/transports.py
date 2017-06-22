75ab2e0d9690aaf7835f577cc1fdaa2feeb96a99
David Silva <zgrav@null.net>
2017-06-22 16:57:36 +0200

    if zero packet, make it empty string

--- socketIO_client/transports.py
+++ socketIO_client/transports.py
@@ -152,13 +152,15 @@ class WebsocketTransport(AbstractTransport):
             if len(packet_text)==0:
                 print "zero packet"
                 #raise PacketError('zero size packet')
+                packet_text = ""
+                engineIO_packet_type, engineIO_packet_data = parse_packet_text(packet_text)
+                yield engineIO_packet_type, engineIO_packet_data
             else:
                 if not isinstance(packet_text, six.binary_type):
                     packet_text = packet_text.encode('utf-8')
                 engineIO_packet_type, engineIO_packet_data = parse_packet_text(
                     packet_text)
                 yield engineIO_packet_type, engineIO_packet_data
-                
         except WebSocketTimeoutException as e:
             raise TimeoutError('recv timed out (%s)' % e)
         except SSLError as e:
