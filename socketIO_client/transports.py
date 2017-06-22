a1cf409f967d1b7a2dcfd29db11c88027da16749
David Silva <zgrav@null.net>
2017-06-22 16:06:15 +0200

    add more verifications to packet_text

--- socketIO_client/transports.py
+++ socketIO_client/transports.py
@@ -143,7 +143,10 @@ class WebsocketTransport(AbstractTransport):
 
     def recv_packet(self):
         try:
-            packet_text = self._connection.recv()
+            packet_text = None
+            while packet_text is None:
+                packet_text = self._connection.recv()
+                 if packet_text is None: time.sleep(.05)
             if type(packet_text)!=str: packet_text=str(packet_text)
             if len(packet_text)==0:
                 print "zero packet"
