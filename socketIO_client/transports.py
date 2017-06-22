7a3e300cdefcf43d5f1ccbabe9e90b3516f28f96
David Silva <zgrav@null.net>
2017-06-22 15:08:17 +0200

    add verification for zero size packets

--- socketIO_client/transports.py
+++ socketIO_client/transports.py
@@ -19,7 +19,7 @@ by running the following commands:
 yes | pip uninstall websocket websocket-client
 pip install -U websocket-client""")
 
-from .exceptions import ConnectionError, TimeoutError
+from .exceptions import ConnectionError, TimeoutError, PacketError
 from .parsers import (
     encode_engineIO_content, decode_engineIO_content,
     format_packet_text, parse_packet_text)
@@ -144,6 +144,10 @@ class WebsocketTransport(AbstractTransport):
     def recv_packet(self):
         try:
             packet_text = self._connection.recv()
+            if type(packet_text)!=str: packet_text=str(packet_text)
+            if len(packet_text)==0:
+                print "zero packet"
+                raise PacketError('zero size packet')
         except WebSocketTimeoutException as e:
             raise TimeoutError('recv timed out (%s)' % e)
         except SSLError as e:
